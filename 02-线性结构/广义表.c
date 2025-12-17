d74dfea3ce803a3dbf6a3c9597de35a30e40e12e
SleePerwtm <wtm2785419816@outlook.com>
2025-12-17 22:17:17 +0800

    feat: 添加广义表数据结构实现
    - 定义了广义表的节点结构体 GNode
    - 包含标志域、数据域和指向下一个节点的指针
    - 使用联合体实现子表指针和单元素数据的共用存储

---
+++ 02-线性结构/广义表.c
@@ -0,0 +1,10 @@
+typedef struct GNode* GList;
+typedef double ElementType;
+struct GNode {
+    int tag; // 标志域，0表示顶点，1表示广义表
+    union {  /* 子表指针域与单元素数据域共用存储空间 */
+        ElementType data;
+        GList sub_list;
+    } URegion;
+    GList next; // 指向下一个结点的指针
+};
