32488451d62f2946631c38bdb227ba7af46c4d35
SleePerwtm <wtm2785419816@outlook.com>
2025-12-18 17:01:04 +0800

    feat: 添加栈的顺序存储cpp实现
    - 新增栈的顺序存储实现，包含基本操作如入栈和出栈
    - 实现了判断栈是否为空和是否为满的功能
    - 使用C++语言编写，包含必要的头文件和数据结构定义

--- 02-线性结构/堆栈顺序存储.cpp
+++ 02-线性结构/堆栈顺序存储.cpp
@@ -1,30 +1,25 @@
-#include <math.h>
-#include <stdio.h>
+#include <iostream>
 
-#define MAXSIZE (int)1e4
+const int kMaxSize = 100; // 栈的最大容量
 
-typedef struct SNode* Stack;
-typedef double ElementType;
-
-#define bool _Bool
-#define true 1
-#define false 0
+using Stack = struct SNode*;
+using ElementType = double;
 
 struct SNode {
-    ElementType data[MAXSIZE]; // 储存的数据
-    int top;                   // 栈顶指针
+    ElementType data[kMaxSize]; // 储存的数据
+    int top;                    // 栈顶指针
 };
 
 /* 判断栈是否为空 */
 bool isEmpty(Stack S) { return S->top == -1; }
 
 /* 判断栈是否为满 */
-bool isFull(Stack S) { return S->top == MAXSIZE - 1; }
+bool isFull(Stack S) { return S->top == kMaxSize - 1; }
 
 /* 入栈 */
 bool push(ElementType X, Stack S) {
     if (isFull(S)) { /* 栈满 */
-        printf("the stack is full\n");
+        std::cout << "the stack is full\n";
         return false;
     }
     S->data[++S->top] = X; // 入栈
@@ -34,7 +29,7 @@ bool push(ElementType X, Stack S) {
 /* 出栈 */
 ElementType pop(Stack S) {
     if (isEmpty(S)) { /* 栈空 */
-        printf("the stack is empty\n");
+        std::cout << "the stack is empty\n";
         return NAN;
     }
     ElementType X = S->data[S->top--]; // 出栈
