012b4e1b55d14bbccdf43b469fc08ad256514572
SleePerwtm <wtm2785419816@outlook.com>
2025-12-18 16:56:36 +0800

    feat: 添加广义表的结构定义，使用cpp
    - 新增广义表的结构体定义，包括节点的标志域、数据域和指向下一个节点的指针。
    - 使用联合体实现子表指针域与单元素数据域的共用存储空间。

--- 02-线性结构/广义表.cpp
+++ 02-线性结构/广义表.cpp
@@ -1,5 +1,5 @@
-typedef struct GNode* GList;
-typedef double ElementType;
+using GList = struct GNode*;
+using ElementType = double;
 struct GNode {
     int tag; // 标志域，0表示顶点，1表示广义表
     union {  /* 子表指针域与单元素数据域共用存储空间 */
